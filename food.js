1f75cedf98b180e1843b04942e98cd85181726d4
sujoyghosh18 <69069562+sujoyghosh18@users.noreply.github.com>
2021-01-27 21:21:30 +0530

    Update food.js

--- food.js
+++ food.js
@@ -23,16 +23,25 @@ class Food {
       return this.foodStock;
     }
 
-    display(){
-      var x=80,y=100;
-      
-      imageMode(CENTER);
-      image(this.image,750,220,70,70);
-      
-      if(this.foodStock!=0){
+    Sujoy Ghosh, [26.01.21 19:35]
+display(){
+        background(46,139,87);
+
+        fill(255,255,254);
+        textSize(15);
+        if(lastFed>=12){
+            text("Last Feed : "+ lastFed%12 + " PM", 50,30);
+        }else if(lastFed==0){
+            text("Last Feed : 12 AM",50,30);
+        }else{
+            text("Last Feed : "+ lastFed + " AM", 50,30);
+        }
+        var x=70,y=100; 
+        imageMode(CENTER);
+        if(this.foodStock!=0){
         for(var i=0;i<this.foodStock;i++){
           if(i%10==0){
-            x=80;
+            x=70;
             y=y+50;
           }
           image(this.image,x,y,50,50);
@@ -40,4 +49,17 @@ class Food {
         }
       }
     }
+
+    bedroom(){
+        background(bedroom,550,500);  
+    }
+      
+    garden(){
+        background(garden,550,500);  
+    } 
+
+    washroom(){
+        background(washroom,550,500); 
+    }
+}
 }
